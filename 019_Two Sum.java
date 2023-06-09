b552ca10dba122017f1db013ef83189501726d0e
Rishi Singh <rs3949427@gmail.com>
2023-06-09 20:35:35 +0530

    Create 019_Two Sum.java

---
+++ 019_Two Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+
+            if(map.containsKey(diff)){
+                return new int[] {i, map.get(diff)};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{-1,-1};
+    }
+}
